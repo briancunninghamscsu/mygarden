459620bed2dcbfca8933662be92cc93ff5749ad4
Brian C <cubr0702@stcloudstate.edu>
2018-03-13 15:02:11 -0500

    added like two dozen activities; one educational and one settings for each parameter. they are unlinked at this point

---
+++ GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/co2learn.java
@@ -0,0 +1,13 @@
+package com.example.briancunningham.gardenbeta.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class co2learn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_co2learn);
+    }
+}
---
+++ GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/lightheighttolerances.java
@@ -0,0 +1,13 @@
+package com.example.briancunningham.gardenbeta.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lightheighttolerances extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lightheighttolerances);
+    }
+}
