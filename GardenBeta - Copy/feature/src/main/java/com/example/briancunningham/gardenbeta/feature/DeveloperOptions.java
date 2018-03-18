b7a83c0d4009d3f8ce4b6828bf9996611f027cef
Brian C <cubr0702@stcloudstate.edu>
2018-03-18 18:44:40 -0500

    Created CClient.java and it did not throw errors. will continue uncommenting and trying to network w/teraterm

--- GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/DeveloperOptions.java
+++ GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/DeveloperOptions.java
@@ -8,21 +8,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class DeveloperOptions extends AppCompatActivity {
+
+    //private CClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer_options);
+
+        //mClient = new CClient();
+        //Thread myThready = new Thread(mClient);
+        //myThready.start();
+
         Button sendastringbutton = (Button) findViewById(R.id.button_send_string);
         sendastringbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "You hopefully transmitted a string", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                //myIntent.putExtra("key", value); //Optional parameters
-                //Intent myIntent = new Intent(DeveloperOptions.this, DeveloperOptions.class);
-                //DeveloperOptions.this.startActivity(myIntent);
+
+                //Socket socket = new Socket("10.0.78.75", 50505);
+
+                //OutputStream out = socket.getOutputStream();
+                //PrintWriter output = new PrintWriter(out);
+
+/*
+                mStatusText.setText("Sending Data to PC");
+                output.println("Hello from Android");
+                out.flush();out.close();
+                mStatusText.setText("Data sent to PC");
+
+                socket.close();
+                mStatusText.setText("Socket closed");
+*/
             }
         });
 
