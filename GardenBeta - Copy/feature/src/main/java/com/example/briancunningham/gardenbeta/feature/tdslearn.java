344899f27abd586a62510bd36dab4b26a8befc6b
Brian C <cubr0702@stcloudstate.edu>
2018-03-18 13:42:16 -0500

    added navigation to the O2, CO2, and Light Height activities/learning/setting

--- GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/tdslearn.java
+++ GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/tdslearn.java
@@ -3,11 +3,11 @@ package com.example.briancunningham.gardenbeta.feature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class tdspH extends AppCompatActivity {
+public class tdslearn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tdsp_h);
+        setContentView(R.layout.activity_tdslearn);
     }
 }
