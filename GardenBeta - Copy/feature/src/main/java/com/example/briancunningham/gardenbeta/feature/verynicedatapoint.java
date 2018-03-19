7d2e22e46f53259bf1cfe8e189777c6c8975a04d
Brian C <cubr0702@stcloudstate.edu>
2018-03-19 15:25:26 -0500

    created data point class

---
+++ GardenBeta - Copy/feature/src/main/java/com/example/briancunningham/gardenbeta/feature/verynicedatapoint.java
@@ -0,0 +1,41 @@
+package com.example.briancunningham.gardenbeta.feature;
+
+/**
+ * Created by briancunningham on 3/19/2018.
+ */
+
+public class verynicedatapoint {
+
+        private float airtemplevel;
+        private float ambienthumiditylevel;
+        private float canopyheightlevel;
+        private float co2level;
+        private float dolevel;
+        private float lightheight;
+        private float o2level;
+        private float orplevel;
+        private float tdslevel;
+        private float phlevel;
+        private float solutiontemplevel;
+        private boolean reservoirs;
+
+        public verynicedatapoint(float airtemplevel, float ambienthumiditylevel, float canopyheightlevel, float co2level, float dolevel, float lightheight, float o2level, float orplevel, float tdslevel, float phlevel, float solutiontemplevel, boolean reservoirs ){
+            this.airtemplevel=airtemplevel;
+            this.ambienthumiditylevel=ambienthumiditylevel;
+            this.canopyheightlevel=canopyheightlevel;
+            this.co2level=co2level;
+            this.canopyheightlevel=canopyheightlevel;
+            this.dolevel=dolevel;
+            this.lightheight=lightheight;
+            this.o2level=o2level;
+            this.orplevel=orplevel;
+            this.tdslevel=tdslevel;
+            this.phlevel=phlevel;
+            this.solutiontemplevel=solutiontemplevel;
+            this.reservoirs=reservoirs;
+            
+
+
+        }
+
+}
